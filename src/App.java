e378517ddd84c19766d220609e1ec9f6aada5787
mbemdude <ala52798@gmail.com>
2023-05-08 19:43:27 +0800

    Demo Final

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        Mahasiswa adul = new Mahasiswa();
+        Mahasiswa bejo = new Mahasiswa();
+
+        adul.npm = "001";
+        adul.nama = "Adul";
+
+        bejo.npm = "002";
+        bejo.nama = "Bejo bin Subejo";
+        
+    }
+}
