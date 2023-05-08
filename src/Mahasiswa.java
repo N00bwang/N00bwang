657bdb57aa4c6375dc92ce6258defc94d0e020a0
mbemdude <ala52798@gmail.com>
2023-05-08 19:35:06 +0800

    Hello Mahasiswa

---
+++ src/Mahasiswa.java
@@ -0,0 +1,15 @@
+public class Mahasiswa {
+    // Atribut
+    String npm;
+    String nama;
+    int jumlahSks;
+    double ipk;
+
+    void login() {
+        System.out.println("Mahasiswa Masuk");
+    }
+
+    void cetakKRS() {
+        System.out.println("Cetak KRS");
+    }
+}
